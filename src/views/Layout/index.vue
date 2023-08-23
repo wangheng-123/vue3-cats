656d8d4d3de9026f288993bbde11d1019a514b0f
wang <11828703+wangheng126@user.noreply.gitee.com>
2023-08-23 13:52:44 +0800

    layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,17 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-  <div>
-    我是首页
-    <!--二级路由出口-->
-    <router-view/>
-  </div>
+  <LayoutNav/>
+  <LayoutHeader/>
+  <RouterView/>
+  <LayoutFooter/>
 </template>
-
-<!--<script>-->
-<!--export default {-->
-<!--  name: "index"-->
-<!--}-->
-<!--</script>-->
-
-<!--<style scoped>-->
-
-<!--</style>-->
