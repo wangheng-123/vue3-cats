0ff9f5524d269c6539863c8ed39f88f7dfcc0307
wang <11828703+wangheng126@user.noreply.gitee.com>
2023-08-24 11:07:43 +0800

    home-goodsItem组件封装

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -2,6 +2,7 @@
 import HomePanel from './HomePanel.vue'
 import {onMounted, ref} from "vue";
 import {getGoodsAPI} from "@/apis/home";
+import GoodsItem from "@/views/Home/components/GoodsItem.vue";
 const goodsProduct = ref([])
 const getGoods = async () => {
   const { result } = await getGoodsAPI()
@@ -23,12 +24,13 @@ onMounted( ()=> getGoods() )
         </RouterLink>
         <ul class="goods-list">
           <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy="good.picture" alt=""  src=""/>
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+            <GoodsItem :goods="good"/>
+<!--            <RouterLink to="/" class="goods-item">-->
+<!--              <img v-img-lazy="good.picture" alt=""  src=""/>-->
+<!--              <p class="name ellipsis">{{ good.name }}</p>-->
+<!--              <p class="desc ellipsis">{{ good.desc }}</p>-->
+<!--              <p class="price">&yen;{{ good.price }}</p>-->
+<!--            </RouterLink>-->
           </li>
         </ul>
       </div>
@@ -144,41 +146,7 @@ onMounted( ()=> getGoods() )
       }
     }
 
-    .goods-item {
-      display: block;
-      width: 220px;
-      padding: 20px 30px;
-      text-align: center;
-      transition: all .5s;
 
-      &:hover {
-        transform: translate3d(0, -3px, 0);
-        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-      }
-
-      img {
-        width: 160px;
-        height: 160px;
-      }
-
-      p {
-        padding-top: 10px;
-      }
-
-      .name {
-        font-size: 16px;
-      }
-
-      .desc {
-        color: #999;
-        height: 29px;
-      }
-
-      .price {
-        color: $priceColor;
-        font-size: 20px;
-      }
-    }
   }
 }
 </style>
