c8faddcc934888ee2ca9240f75c79b49cce64bd6
wang <11828703+wangheng126@user.noreply.gitee.com>
2023-08-25 17:07:35 +0800

    二级分类-列表无线加载实现

--- src/directives/index.js
+++ src/directives/index.js
@@ -9,11 +9,11 @@ export const lazyPlugin = {
             mounted(el,binding){
                 //el: 指令绑定的元素 img
                 //binding: binding.value 指令等于号后面绑定的表达式的值 图片url
-                console.log(el,binding.value)
+                // console.log(el,binding.value)
                 const {stop} = useIntersectionObserver(
                     el,
                     ([{ isIntersecting }]) => {
-                        console.log(isIntersecting)
+                        // console.log(isIntersecting)
                         if (isIntersecting){
                             el.src = binding.value
                             stop()
--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -30,6 +30,27 @@ const getGoodList = async ()=>{
 
 onMounted(()=>getGoodList())
 
+//tab切换回调
+const tabChange =()=>{
+  console.log(reqData.value.sortField)
+  reqData.value.page = 1
+  getGoodList()
+}
+
+//加载更多
+const disabled = ref(false)
+const load=async ()=>{
+  console.log("加载更多数据")
+  //获取下一页数据
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value,...res.result.items]
+  //加载完毕，停止监听
+  if (res.result.items.length===0){
+    disabled.value = true
+  }
+}
+
 </script>
 
 <template>
@@ -44,12 +65,12 @@ onMounted(()=>getGoodList())
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
         <!-- 商品列表-->
         <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
       </div>
